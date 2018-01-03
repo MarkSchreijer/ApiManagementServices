8d6e291f50467759495b50556b8e3559aade3bd0
Mark Schreijer <mschreijer@newnexusmobile.nl>
2018-01-03 21:49:29 +0100

    Initial

---
+++ ApiManagement.Services/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using ApiManagement.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace ApiManagement.Services.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public List<Customer> Get()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = Guid.NewGuid(), FirstName = "Arjen", LastName = "Robben" },
+                new Customer { Id = Guid.NewGuid(), FirstName = "Lionel", LastName = "Messi" },
+                new Customer { Id = Guid.NewGuid(), FirstName = "Cristiano", LastName = "Ronaldo" }
+            };
+        }
+    }
+}
---
+++ ApiManagement.Services/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace ApiManagement.Services.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Ok("Mijn eerste API in Api Management");
+        }
+    }
+}
